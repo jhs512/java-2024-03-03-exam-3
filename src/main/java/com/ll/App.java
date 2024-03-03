aaf3523354136595c118ae992c9f29938ddb1004
jhs512 <jangka512@gmail.com>
2024-03-03 09:38:45 +0900

    work

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,8 +1,17 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class App {
     public void run() {
         System.out.println("계산기, 시작!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("명령) ");
+        String cmd = scanner.nextLine();
+
+        System.out.println("입력한 명령: " + cmd);
 
         System.out.println("계산기, 종료!");
     }
