97c877dd89e9cc2734298e4432a66b697ab7f58a
jhs512 <jangka512@gmail.com>
2024-03-03 09:44:58 +0900

    work

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -7,10 +7,14 @@ public class App {
         System.out.println("계산기, 시작!");
 
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("명령) ");
-            String cmd = scanner.nextLine();
+            while (true) {
+                System.out.print("명령) ");
+                String cmd = scanner.nextLine();
 
-            System.out.println("입력한 명령: " + cmd);
+                if (cmd.equals("종료")) break;
+
+                System.out.println("입력한 명령: " + cmd);
+            }
         }
 
         System.out.println("계산기, 종료!");
